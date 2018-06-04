c279bb3d65debd017ddd355b401a277ea7519c68
Mr.Ilie (Aka MrTeofil) <utiblingb@yahoo.com>
2018-06-04 22:58:43 +0300

    previous version was much better, i think

--- EOG-Eye-Tracker.ino
+++ EOG-Eye-Tracker.ino
@@ -7,6 +7,17 @@
 #include "main.h"
 
 
+#define SAMPLERATE   200                // Frecventa de esantionare semnal / canal
+#define READINGS     20                 // Interval pt calculul mediei semnalului   
+#define INACTIVE_CMD 20                 // Interval inactivare comanda noua t = 0.1sec
+#define READING_INT  5                  // Durata in ms intre esantionari succesive 
+
+#define XTH_LIMIT_PLUS 60               // Limitele de variatie a pragurilor
+#define XTH_LIMIT_MINUS 60
+#define YTH_LIMIT_PLUS 60
+#define YTH_LIMIT_MINUS 60
+ 
+
 unsigned int x_threshold_up = 0;        // Praguri detectie comparatoare
 unsigned int x_threshold_dn = 0; 
 unsigned int y_threshold_up = 0; 
